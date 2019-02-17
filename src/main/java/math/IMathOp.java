8ace0de632658e4686d695ae85baa3ac064b9191
romansen47 <romansen47@gmail.com>
2019-02-17 14:03:48 +0100

    initial commit

---
+++ src/main/java/math/IMathOp.java
@@ -0,0 +1,70 @@
+package math;
+
+public interface IMathOp {
+
+	
+	 default public double Abs (double Scalar) {
+		 if (Scalar>=0) 
+			 return Scalar; 
+		 else return -Scalar;
+	 }
+	 
+	 default public double SignumFunction (double Int) { 
+		 if (Int == 0) 
+			 return 0; 
+		 else { 
+			 if (Int < 0) 
+				 return -1; 
+			 else return 1;
+			 }
+	 }
+	 
+	 default public double ScalarProduct (double[] VEC1, double[] VEC2){ 
+		 double skalprod = VEC1[0]*VEC2[0]+ VEC1[1]*VEC2[1]; 
+		 return skalprod;
+	 }
+	 
+	 default public double MagnitudeOfVector(double[] VEC){
+		 return SQRT(ScalarProduct(VEC,VEC));
+	 } /* Usage of native Math.sqrt-function weirdly lacks precision */
+	
+	 default public double[] ScalarMultiplication (double skalar, double[] VECTOR){
+		 double[] skalarmult = new double[2]; 
+		 skalarmult[0] = skalar*VECTOR[0];
+		 skalarmult[1] = skalar*VECTOR[1];
+		 return skalarmult;
+	 }
+	 
+	 default public double[] UnitVector (double[] VEC){
+		 return ScalarMultiplication(1/MagnitudeOfVector(VEC),VEC);
+	 }
+	 
+	 default public double[] AdditionOfVectors (double[] VEC1, double[] VEC2){
+		 double[] addarrays = new double[2]; 
+		 addarrays[0] = VEC1[0]+VEC2[0];
+		 addarrays[1] = VEC1[1]+VEC2[1];
+		 return addarrays;
+	 }
+	
+	 default public double SQRT(double Square){
+		  return(Math.sqrt(Square));
+		  /*double tmp=Square/2;
+		  while (Abs(tmp*tmp-Square)>Config.SquareRootCorrectness){
+		   tmp=0.5*(tmp+Square/tmp); 
+		  }
+		  return tmp;*/
+	 }
+	 
+	 default public double[] ReversalOfVector (double[] VEC){
+		 return ScalarMultiplication(-1,VEC);
+	 }
+	
+	 default public double[] Projection(double[] X, double [] Y){
+		 return ScalarMultiplication(ScalarProduct(X,Y),ScalarMultiplication(1/MagnitudeOfVector(Y),Y));
+	 }
+	
+	 default public double[] ProjectionComplement(double[] X, double[] Y){
+		 return AdditionOfVectors(X,ReversalOfVector(Projection(X,Y)));
+	 }
+	 
+}
---
+++ src/main/java/math/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author RoMansen
+ *
+ *	This package implements final dimensional mathematical methods. 
+ *
+ *  It is meant to be an alternative to the standard java math library.
+ *
+ */
+package math;
